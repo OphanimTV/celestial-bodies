b13ec9d3e924ed3635e01ffd873046329a8c5fdd
OphanimTV <103788075+OphanimTV@users.noreply.github.com>
2022-07-11 16:29:56 -0400

    Update number_guess.sh

--- Number Guessing Game/number_guess.sh
+++ Number Guessing Game/number_guess.sh
@@ -16,12 +16,16 @@ then
     let NUMBER_OF_GUESSES++
 
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $NUMBER. Nice job!"
+    
+    #Update number of games played
+    let GAMES_PLAYED++
+    RESULT=$($PSQL "UPDATE users SET games_played = '$GAMES_PLAYED' WHERE username = '$USERNAME'")
 
     #Update best game yet
-    BEST_GAME=$($PSQL "SELECT best_game FROM guesser WHERE username = '$USERNAME'")
-     if [[ $BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
+    BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username = '$USERNAME'")
+    if [[ $BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
     then
-      UPDATE_BEST_GAME_RESULT=$($PSQL "UPDATE guesser SET best_game = '$NUMBER_OF_GUESSES' WHERE username = '$USERNAME'")
+      RESULT=$($PSQL "UPDATE users SET best_game = '$NUMBER_OF_GUESSES' WHERE username = '$USERNAME'")
     fi
       
   else if [[ $GUESS > $NUMBER ]]
@@ -44,18 +48,15 @@ fi
 echo "Enter your username:"
 read USERNAME
 
-IFS="|" read GAMES_PLAYED BEST_GAME <<< $($PSQL "SELECT games_played, best_game FROM guesser WHERE username = '$USERNAME'")
+IFS="|" read GAMES_PLAYED BEST_GAME <<< $($PSQL "SELECT games_played, best_game FROM users WHERE username = '$USERNAME'")
 
-if [[ -z $GAMES_PLAYED ]]
+#If the user doesn't exist, create the user.
+if [[ $GAMES_PLAYED -eq 0 ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  INSERT_USER_RESULT=$($PSQL "INSERT INTO guesser(username, games_played, best_game) VALUES('$USERNAME', 1, 0)")
+  RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
 else
-  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-
-  #Update number of games played
-  let GAMES_PLAYED++   
-  UPDATE_GAMES_PLAYED_RESULT=$($PSQL "UPDATE guesser SET games_played = '$GAMES_PLAYED' WHERE username = '$USERNAME'") 
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."   
 fi
 
 echo "Guess the secret number between 1 and 1000:"
