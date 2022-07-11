7915fbbad8ff059fc8d4638f095755c8900aef54
OphanimTV <103788075+OphanimTV@users.noreply.github.com>
2022-07-11 17:36:58 -0400

    Update number_guess.sql

--- Number Guessing Game/number_guess.sql
+++ Number Guessing Game/number_guess.sql
@@ -48,7 +48,7 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.users (
-    username character varying(16) NOT NULL,
+    username character varying(30) NOT NULL,
     games_played integer DEFAULT 0 NOT NULL,
     best_game integer DEFAULT 0 NOT NULL
 );
